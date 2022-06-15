6d1764ed82b4578fbb0167e0d01e3f39795352c5
Johanna Mantilla <johanna@propeldata.com>
2022-06-14 19:47:29 -0500

    chore: Update tests

--- propel/provider_test.go
+++ propel/provider_test.go
@@ -1,19 +1,23 @@
 package propel
 
 import (
+	"context"
 	"os"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 )
 
-var testAccProviders map[string]*schema.Provider
 var testAccProvider *schema.Provider
+var testAccProviderFactories map[string]func() (*schema.Provider, error)
 
 func init() {
 	testAccProvider = Provider()
-	testAccProviders = map[string]*schema.Provider{
-		"propel": testAccProvider,
+	testAccProviderFactories = map[string]func() (*schema.Provider, error){
+		"propel": func() (*schema.Provider, error) {
+			return testAccProvider, nil
+		},
 	}
 }
 
@@ -33,5 +37,10 @@ func testAccPreCheck(t *testing.T) {
 	}
 	if err := os.Getenv("PROPEL_CLIENT_SECRET"); err == "" {
 		t.Fatal("PROPEL_CLIENT_SECRET must be set for acceptance tests")
+	}
+
+	err := testAccProvider.Configure(context.Background(), terraform.NewResourceConfigRaw(nil))
+	if err != nil {
+		t.Fatal(err)
 	}
 }
--- propel/resource_datasource_test.go
+++ propel/resource_datasource_test.go
@@ -2,37 +2,51 @@ package propel
 
 import (
 	"context"
+	"errors"
 	"fmt"
+	"regexp"
 	"testing"
-
-	cms "github.com/propeldata/terraform-provider/graphql_client"
 
 	"github.com/Khan/genqlient/graphql"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+
+	pc "github.com/propeldata/terraform-provider/graphql_client"
 )
 
-func TestCreateDataSource(t *testing.T) {
-
+func TestAccPropelDataSourceBasic(t *testing.T) {
 	resource.Test(t, resource.TestCase{
-		PreCheck:     func() { testAccPreCheck(t) },
-		Providers:    testAccProviders,
-		CheckDestroy: testAccCheckPropelDataSourceDestroy,
+		PreCheck:          func() { testAccPreCheck(t) },
+		ProviderFactories: testAccProviderFactories,
+		CheckDestroy:      testAccCheckPropelDataSourceDestroy,
 		Steps: []resource.TestStep{
 			{
-				Config: testAccCheckPropelDataSource(),
+				Config:      testAccCheckPropelDataSourceConfigBasic(),
+				ExpectError: regexp.MustCompile(`DataSource in BROKEN status`),
 				Check: resource.ComposeTestCheckFunc(
 					testAccCheckPropelDataSourceExists("propel_datasource.new"),
+					resource.TestCheckResourceAttr("propel_datasource.new", "unique_name", "test"),
+					resource.TestCheckResourceAttr("propel_datasource.new", "status", "BROKEN"),
 				),
 			},
 		},
 	})
 }
 
-func testAccCheckPropelDataSource() string {
+func testAccCheckPropelDataSourceConfigBasic() string {
 	return fmt.Sprintf(`
 	resource "propel_datasource" "new" {
-		database = test
+		unique_name = "test"
+
+		connection_settings {
+			account = "snowflake-account"
+			database = "snowflake-database"
+			warehouse = "snowflake-warehouse"
+			schema = "snowflake-schema"
+			role = "snowflake-role"
+			username = "snowflake-username"
+			password = "snowflake-password"
+		}
 	}`)
 }
 
@@ -46,7 +60,7 @@ func testAccCheckPropelDataSourceDestroy(s *terraform.State) error {
 
 		dataSourceID := rs.Primary.ID
 
-		_, err := cms.DeleteDataSource(context.Background(), c, dataSourceID)
+		_, err := pc.DeleteDataSource(context.Background(), c, dataSourceID)
 		if err != nil {
 			return err
 		}
@@ -60,11 +74,11 @@ func testAccCheckPropelDataSourceExists(n string) resource.TestCheckFunc {
 		rs, ok := s.RootModule().Resources[n]
 
 		if !ok {
-			return fmt.Errorf("Not found: %s", n)
+			return fmt.Errorf("not found: %s", n)
 		}
 
 		if rs.Primary.ID == "" {
-			return fmt.Errorf("No DataSourceID set")
+			return errors.New("no DataSourceID set")
 		}
 
 		return nil
