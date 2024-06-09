f42cee14d0edc117118e25023299ebd8aadb0687
Johanna Mantilla <johanna@propeldata.com>
2024-06-09 08:52:23 -0500

    chore: Put back the webhook Data Source example

--- examples/resources/propel_data_source/resource.tf
+++ examples/resources/propel_data_source/resource.tf
@@ -18,3 +18,47 @@ resource "propel_data_source" "my_data_source" {
     password  = var.snowflake_password
   }
 }
+
+variable "http_basic_auth_password" {
+  type = string
+  sensitive = true
+}
+
+resource "propel_data_source" "my_webhook_data_source" {
+  unique_name = "MyWebhookDataPool"
+  description = "This is an example of a Webhook Data Source"
+  type        = "WEBHOOK"
+  webhook_connection_settings {
+    timestamp = "event_timestamp"
+    unique_id = "event_id"
+    tenant = "customer_id"
+    column {
+      name = "event_id"
+      type = "STRING"
+      nullable = false
+      json_property = "event_id"
+    }
+    column {
+      name = "customer_id"
+      type = "STRING"
+      nullable = false
+      json_property = "customer_id"
+    }
+    column {
+      name = "event_timestamp"
+      type = "TIMESTAMP"
+      nullable = false
+      json_property = "event_timestamp"
+    }
+    column {
+      name = "customer_name"
+      type = "STRING"
+      nullable = true
+      json_property = "customer_name"
+    }
+    basic_auth {
+      username = "foo"
+      password = var.http_basic_auth_password
+    }
+  }
+}
