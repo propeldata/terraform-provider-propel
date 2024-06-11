167b0e2fa07403c1da1ff13a529bdf5450817b6e
Johanna Mantilla <johanna@propeldata.com>
2024-06-11 16:03:48 -0500

    chore: Allow read and delete policies

--- propel/resource_policy.go
+++ propel/resource_policy.go
@@ -4,9 +4,12 @@ import (
 	"context"
 	"errors"
 
+	"github.com/Khan/genqlient/graphql"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
+
+	pc "github.com/propeldata/terraform-provider-propel/propel_client"
 )
 
 func resourcePolicy() *schema.Resource {
@@ -49,14 +52,44 @@ func resourcePolicyCreate(_ context.Context, _ *schema.ResourceData, _ any) diag
 	return diag.FromErr(errors.New("use propel_data_pool_access_policy resource instead"))
 }
 
-func resourcePolicyRead(_ context.Context, _ *schema.ResourceData, _ any) diag.Diagnostics {
-	return diag.FromErr(errors.New("use propel_data_pool_access_policy resource instead"))
+func resourcePolicyRead(ctx context.Context, d *schema.ResourceData, m any) diag.Diagnostics {
+	c := m.(graphql.Client)
+
+	response, err := pc.Policy(ctx, c, d.Id())
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	d.SetId(response.Policy.Id)
+
+	if err := d.Set("type", response.Policy.Type); err != nil {
+		return diag.FromErr(err)
+	}
+
+	if err := d.Set("application", response.Policy.Application.Id); err != nil {
+		return diag.FromErr(err)
+	}
+
+	if err := d.Set("metric", response.Policy.Metric.Id); err != nil {
+		return diag.FromErr(err)
+	}
+
+	return nil
 }
 
 func resourcePolicyUpdate(_ context.Context, _ *schema.ResourceData, _ any) diag.Diagnostics {
 	return diag.FromErr(errors.New("use propel_data_pool_access_policy resource instead"))
 }
 
-func resourcePolicyDelete(_ context.Context, _ *schema.ResourceData, _ any) diag.Diagnostics {
-	return diag.FromErr(errors.New("use propel_data_pool_access_policy resource instead"))
+func resourcePolicyDelete(ctx context.Context, d *schema.ResourceData, meta any) diag.Diagnostics {
+	c := meta.(graphql.Client)
+
+	_, err := pc.DeletePolicy(ctx, c, d.Id())
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	d.SetId("")
+
+	return nil
 }
